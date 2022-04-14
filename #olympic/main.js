b322efcee1f5e9168c52be86013f41885fd95dfe
mahi8gupta <103641179+mahi8gupta@users.noreply.github.com>
2022-04-14 16:48:24 +0530

    Add files via upload

---
+++ #olympic/main.js
@@ -0,0 +1,35 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = blue;
+ctx.lineWidth = 3;
+ctx.arc(240, 210, 40, 0, 2*Math.PI);
+stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 3;
+ctx.arc(300, 210, 40, 0, 2*Math.PI);
+stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 3;
+ctx.arc(340, 210, 40, 0, 2*Math.PI);
+stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 3;
+ctx.arc(400, 210, 40, 0, 2*Math.PI);
+stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 3;
+ctx.arc(440, 210, 40, 0, 2*Math.PI);
+stroke();
+
+
+
